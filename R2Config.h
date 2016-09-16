17c50f5ae9afde56e10cc303085cbe12423df5f4
ethan.zhou <ethan.zhou@netis.com.cn>
2016-09-16 18:34:45 +0800

    create project

---
+++ R2Config.h
@@ -0,0 +1,4 @@
+// the configured options and settings for R2
+#define R2_VERSION_MAJOR 1
+#define R2_VERSION_MINOR 0
+
---
+++ main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: 2016年9月10日
+ *      Author: Admin
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int a = 5;
+
+
+
+#endif /* MAIN_H_ */
